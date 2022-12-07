9fad60cc052da06800066863a6c018577399e9d0
the-aziz <aziz.sheikhehpour.id@gmail.com>
2022-12-07 07:31:25 +0330

    Update api.php for add Brand route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(BrandController::class)->group(function () {
+    Route::get('/brands', 'index');
+    Route::post('/brand', 'store');
+    Route::get('/brands/{brand}', 'show');
+    Route::put('/brands/{brand}', 'update');
+    Route::delete('/brands/{brand}', 'destroy');
 });
