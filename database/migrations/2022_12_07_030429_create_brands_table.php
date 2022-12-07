24dbd3f7d9c125d51a9ef0c17647a95190e490c9
the-aziz <aziz.sheikhehpour.id@gmail.com>
2022-12-07 06:58:30 +0330

    Update Brand migration

--- database/migrations/2022_12_07_030429_create_brands_table.php
+++ database/migrations/2022_12_07_030429_create_brands_table.php
@@ -4,17 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->string('image');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -24,7 +26,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('brands');
     }
