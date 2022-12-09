97af376b2c71e61e730ffe8c611597199650925e
the-aziz <aziz.sheikhehpour.id@gmail.com>
2022-12-09 12:24:17 +0330

    Update BrandController.php change store & create methods

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -9,47 +9,40 @@ use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
-
     public function index(): JsonResponse
     {
-        return $this->successResponse(200, Brand::all());
+        return self::successResponse(200, Brand::all());
     }
 
 
-    public function store(Request $request): JsonResponse
+    public function store(Request $request, Brand $brand): JsonResponse
     {
-        Brand::query()->create([
-            'title' => $request->title,
-            'image' => $request->image
-        ]);
-
-        $dataResponse = Brand::query()->orderBy('id', 'desc')->first();
-        return $this->successResponse(
-            201, $dataResponse);
+        $brand->storeBrand($request);
+        $dataResponse = Brand::query()
+            ->orderBy('id', 'desc')
+            ->first();
+        return self::successResponse(201, $dataResponse);
     }
 
 
     public function show(Brand $brand): JsonResponse
     {
-        return $this->successResponse(200, $brand);
+        return self::successResponse(200, $brand);
     }
 
 
     public function update(Request $request, Brand $brand): JsonResponse
     {
-        Brand::query()->update([
-            'title' => $request->title,
-            'image' => $request->image
-        ]);
-
-        return $this->successResponse(
-            200, $brand);
+        $brand->updateBrand($request);
+        return self::successResponse(200, $brand);
     }
 
 
     public function destroy(Brand $brand): JsonResponse
     {
         $brand->delete();
-        return $this->successResponse(200, $brand);
+        return self::successResponse(200, $brand);
     }
+
+
 }
