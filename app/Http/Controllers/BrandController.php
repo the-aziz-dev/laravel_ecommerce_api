30f36a69310be321c14b3dc94995ca191dd90893
the-aziz <aziz.sheikhehpour.id@gmail.com>
2022-12-09 15:47:32 +0330

    Update BrandController.php to use BrandResource

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BrandResource;
 use App\Models\Brand;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -11,7 +12,7 @@ class BrandController extends Controller
 {
     public function index(): JsonResponse
     {
-        return self::successResponse(200, Brand::all());
+        return self::successResponse(200, BrandResource::collection(Brand::all()));
     }
 
 
@@ -21,28 +22,27 @@ class BrandController extends Controller
         $dataResponse = Brand::query()
             ->orderBy('id', 'desc')
             ->first();
-        return self::successResponse(201, $dataResponse);
+        return self::successResponse(201, new BrandResource($dataResponse));
     }
 
 
     public function show(Brand $brand): JsonResponse
     {
-        return self::successResponse(200, $brand);
+        return self::successResponse(200, new BrandResource($brand));
     }
 
 
     public function update(Request $request, Brand $brand): JsonResponse
     {
         $brand->updateBrand($request);
-        return self::successResponse(200, $brand);
+        return self::successResponse(200, new BrandResource($brand));
     }
 
 
     public function destroy(Brand $brand): JsonResponse
     {
         $brand->delete();
-        return self::successResponse(200, $brand);
+        return self::successResponse(200, new BrandResource($brand));
     }
-
 
 }
