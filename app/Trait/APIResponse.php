2c42acb488feab5e5a25e09ba2bedd0b193a7330
the-aziz <aziz.sheikhehpour.id@gmail.com>
2022-12-09 10:20:09 +0330

    Update APIResponse.php change responses method

--- app/Trait/APIResponse.php
+++ app/Trait/APIResponse.php
@@ -6,21 +6,18 @@ use Illuminate\Http\JsonResponse;
 
 trait APIResponse
 {
-    protected static function successResponse($statusCode, $message, $data): JsonResponse
+    protected static function successResponse($statusCode, $data): JsonResponse
     {
         return response()->json([
             'status' => 'successful',
-            'message' => $message,
             'results' => $data,
         ], $statusCode);
-
     }
 
-    protected static function failResponse($statusCode, $message): JsonResponse
+    protected static function failResponse($statusCode): JsonResponse
     {
         return response()->json([
             'status' => 'failed',
-            'message' => $message,
         ], $statusCode);
     }
 }
