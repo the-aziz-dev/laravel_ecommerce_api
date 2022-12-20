276ca231762e6c32fb2eb94a52c254ff362b1dfd
the-aziz <aziz.sheikhehpour.id@gmail.com>
2022-12-20 16:17:08 +0330

    Update Category.p to implement store & update methods

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Request;
 
 class Category extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
+
+    public function storeCategory(Request $request)
+    {
+        $this->query()->create([
+            'title' => $request->title,
+            'parent_id' => $request->parent_id
+        ]);
+    }
+
+    public function updateCategory(Request $request)
+    {
+        $this->update([
+            'title' => $request->title,
+            'parent_id' => $request->parent_id
+        ]);
+    }
 }
