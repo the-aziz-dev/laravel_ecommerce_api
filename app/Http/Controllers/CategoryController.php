55cedcd0d942d283a2a2ac64df0a529a01414f64
the-aziz <aziz.sheikhehpour.id@gmail.com>
2022-12-20 13:11:28 +0330

    Add Category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($category)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $category)
+    {
+        //
+    }
+
+
+    public function destroy($category)
+    {
+        //
+    }
+}
